5bb3a5c1c1155c7c48fd2382b35ad751bc617479
Khitrov Aleksandr <khitrov.av@erkapharm.com>
2025-01-09 18:00:53 +0300

    add alt port for all mobile services

--- config/config.go
+++ config/config.go
@@ -8,12 +8,18 @@ import (
 )
 
 type Config struct {
-	SitePort         string
-	SiteBackendUrl   string
-	MobilePort       string
-	MobileBackendUrl string
-	MonitorPort      string
-	RedisAddr        string
+	SitePort           string
+	SiteBackendUrl     string
+	MobilePortOz       string
+	MobileBackendUrlOz string
+	MobilePortSf       string
+	MobileBackendUrlSf string
+	MobilePortSa       string
+	MobileBackendUrlSa string
+	MobilePortSt       string
+	MobileBackendUrlSt string
+	MonitorPort        string
+	RedisAddr          string
 }
 
 func New() *Config {
@@ -24,11 +30,17 @@ func New() *Config {
 	}
 
 	return &Config{
-		SitePort:         os.Getenv("SITE_PORT"),
-		SiteBackendUrl:   os.Getenv("SITE_BACKEND_URL"),
-		MobilePort:       os.Getenv("MOBILE_PORT"),
-		MobileBackendUrl: os.Getenv("MOBILE_BACKEND_URL"),
-		MonitorPort:      os.Getenv("MONITOR_PORT"),
-		RedisAddr:        os.Getenv("REDIS_ADDR"),
+		SitePort:           os.Getenv("SITE_PORT"),
+		SiteBackendUrl:     os.Getenv("SITE_BACKEND_URL"),
+		MobilePortOz:       os.Getenv("MOBILE_PORT_OZ"),
+		MobileBackendUrlOz: os.Getenv("MOBILE_BACKEND_URL_OZ"),
+		MobilePortSf:       os.Getenv("MOBILE_PORT_SF"),
+		MobileBackendUrlSf: os.Getenv("MOBILE_BACKEND_URL_SF"),
+		MobilePortSa:       os.Getenv("MOBILE_PORT_SA"),
+		MobileBackendUrlSa: os.Getenv("MOBILE_BACKEND_URL_SA"),
+		MobilePortSt:       os.Getenv("MOBILE_PORT_ST"),
+		MobileBackendUrlSt: os.Getenv("MOBILE_BACKEND_URL_ST"),
+		MonitorPort:        os.Getenv("MONITOR_PORT"),
+		RedisAddr:          os.Getenv("REDIS_ADDR"),
 	}
 }
--- mobile/mobile.go
+++ mobile/mobile.go
@@ -11,17 +11,81 @@ import (
 )
 
 func Run(cfg *config.Config, rp repository.Repository) {
-	aLog := logger.NewLogger("logs/mobile/all.log")
-	acLog := logger.NewLogger("logs/mobile/accepted.log")
+	go runOz(cfg, rp)
+	go runSf(cfg, rp)
+	go runSa(cfg, rp)
+	go runSt(cfg, rp)
+}
+
+func runOz(cfg *config.Config, rp repository.Repository) {
+	aLog := logger.NewLogger("logs/mobile/all-oz.log")
+	acLog := logger.NewLogger("logs/mobile/accepted-oz.log")
 
 	h := handler.New(
 		rp,
-		logger.NewHandlerLogger("logs/mobile/handle.log"),
+		logger.NewHandlerLogger("logs/mobile/handle-oz.log"),
 	)
 
 	proxy.New(
-		cfg.MobilePort,
-		cfg.MobileBackendUrl,
+		cfg.MobilePortOz,
+		cfg.MobileBackendUrlOz,
+		echo.New(),
+		h,
+		aLog,
+		acLog,
+	).Run()
+}
+
+func runSf(cfg *config.Config, rp repository.Repository) {
+	aLog := logger.NewLogger("logs/mobile/all-sf.log")
+	acLog := logger.NewLogger("logs/mobile/accepted-sf.log")
+
+	h := handler.New(
+		rp,
+		logger.NewHandlerLogger("logs/mobile/handle-sf.log"),
+	)
+
+	proxy.New(
+		cfg.MobilePortSf,
+		cfg.MobileBackendUrlSf,
+		echo.New(),
+		h,
+		aLog,
+		acLog,
+	).Run()
+}
+
+func runSa(cfg *config.Config, rp repository.Repository) {
+	aLog := logger.NewLogger("logs/mobile/all-sa.log")
+	acLog := logger.NewLogger("logs/mobile/accepted-sa.log")
+
+	h := handler.New(
+		rp,
+		logger.NewHandlerLogger("logs/mobile/handle-sa.log"),
+	)
+
+	proxy.New(
+		cfg.MobilePortSa,
+		cfg.MobileBackendUrlSa,
+		echo.New(),
+		h,
+		aLog,
+		acLog,
+	).Run()
+}
+
+func runSt(cfg *config.Config, rp repository.Repository) {
+	aLog := logger.NewLogger("logs/mobile/all-st.log")
+	acLog := logger.NewLogger("logs/mobile/accepted-st.log")
+
+	h := handler.New(
+		rp,
+		logger.NewHandlerLogger("logs/mobile/handle-st.log"),
+	)
+
+	proxy.New(
+		cfg.MobilePortSt,
+		cfg.MobileBackendUrlSt,
 		echo.New(),
 		h,
 		aLog,
