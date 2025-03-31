37de3ed73c20ad2544e04ae72b52999ac53425f3
Khitrov Aleksandr <khitrov.av@erkapharm.com>
2025-03-31 16:47:11 +0300

    change cookie name

--- site/handler/cookie.go
+++ site/handler/cookie.go
@@ -5,7 +5,7 @@ import "github.com/labstack/echo/v4"
 func (h *Handler) denyCookie(c echo.Context) bool {
 	req := c.Request()
 	for _, cookie := range req.Cookies() {
-		if cookie.Name == "rrpvid" {
+		if cookie.Name == "_ga" {
 			return false
 		}
 	}
