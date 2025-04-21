d5848398c11ef0c7353da4441d1321c0f27262b4
Khitrov Aleksandr <khitrov.av@erkapharm.com>
2025-04-21 13:53:16 +0300

    all log all

--- logger/all.go
+++ logger/all.go
@@ -4,11 +4,11 @@ import "github.com/labstack/echo/v4"
 
 func (l *Logger) Handler(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		uri := c.Request().RequestURI
+		//uri := c.Request().RequestURI
 
-		if uri == "/api/v8/manzana/registration" || uri == "/mirror/manzana/registration" || uri == "/api/v8/ecom-auth/login-sms-prestep" || uri == "/mirror/ecom-auth/login-sms-prestep" {
-			l.logWithFormat(c)
-		}
+		//if uri == "/api/v8/manzana/registration" || uri == "/mirror/manzana/registration" || uri == "/api/v8/ecom-auth/login-sms-prestep" || uri == "/mirror/ecom-auth/login-sms-prestep" {
+		l.logWithFormat(c)
+		//}
 
 		return next(c)
 	}
